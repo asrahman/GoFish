e8922affac7ffd0ccddebe2ee105ee65f1b243c7
Ayesha <asrahman@utexas.edu>
2018-11-05 19:49:08 -0600

    Added constructor and dealCard

--- deck.cpp
+++ deck.cpp
@@ -5,5 +5,24 @@
 #include "deck.h"
 
 Deck::Deck() {
+    myIndex = 0; //initial value of index
 
+    int tempSuit;
+    int tempRank;  //for creating new cards
+    int index = 0;
+    for (tempSuit = 0; tempSuit < 4; tempSuit++) {
+        for (tempRank = 1; tempRank < 14; tempRank++) {
+            myCards[index] = Card(tempRank, Card::Suit(tempSuit));
+            index++;
+        }
+    }
+}
+
+Card Deck::dealCard() {
+    Card tempCard(0,Card::Suit(0));
+    if (size() > 0) {
+        tempCard = myCards[myIndex];
+        myIndex++;
+    }
+    return tempCard;
 }
