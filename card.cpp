aa0adbc60460e93b00489c5357520b448ac1ed48
Ayesha <asrahman@utexas.edu>
2018-11-07 18:49:34 -0600

    Changed outstream

--- card.cpp
+++ card.cpp
@@ -43,23 +43,24 @@ string Card::suitString(Suit s) const {
 }
 
 string Card::rankString(int r) const {
-    string rank;
-    if (r == 1) {
-        rank = 'A';
-    }
-    else if (r == 11) {
-        rank = 'J';
-    }
-    else if (r == 12) {
-        rank = 'Q';
-    }
-    else if (r == 13) {
-        rank = 'K';
-    }
-    else {
-        rank = to_string(r);
-    }
-    return rank;
+    string rankArray[13] = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+    //string rank;
+    //if (r == 1) {
+    //    rank = 'A';
+    //}
+    //else if (r == 11) {
+    //    rank = 'J';
+    //}
+    //else if (r == 12) {
+    //    rank = 'Q';
+    //}
+    //else if (r == 13) {
+    //    rank = 'K';
+    //}
+    //else {
+      //  rank = to_string(r);
+    ///}
+    return rankArray[r-1];
 }
 
 string Card::toString() const {
@@ -72,4 +73,9 @@ bool Card::operator == (const Card &rhs) const {
 
 bool Card::operator!=(const Card &rhs) const {
     return mySuit != rhs.mySuit && myRank != rhs.myRank;
+}
+
+ostream& operator << (ostream& out, const Card& c) {
+    out << c.toString();
+    return out;
 }
